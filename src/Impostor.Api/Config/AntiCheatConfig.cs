e4950f8e3f1a16ab5a6a7756f1e39a3ffaf4f2e9
saverio.ieva <saverio.ieva@poliba.it>
2024-03-06 18:17:24 +0100

    Update dat sessions and change anticheat configuration

--- src/Impostor.Api/Config/AntiCheatConfig.cs
+++ src/Impostor.Api/Config/AntiCheatConfig.cs
@@ -1,11 +1,11 @@
-﻿namespace Impostor.Api.Config
+namespace Impostor.Api.Config
 {
     public class AntiCheatConfig
     {
         public const string Section = "AntiCheat";
 
-        public bool Enabled { get; set; } = true;
+        public bool Enabled { get; set; } = false;
 
-        public bool BanIpFromGame { get; set; } = true;
+        public bool BanIpFromGame { get; set; } = false;
     }
 }
