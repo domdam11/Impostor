4499810b354e019cc821f7d95f1c689b0296f674
saverio.ieva <saverio.ieva@poliba.it>
2025-05-20 10:38:06 +0200

    Integrate annotation interval options from appsettings.json

--- src/Impostor.Plugins.SemanticAnnotator/SemanticAnnotatorPlugin.cs
+++ src/Impostor.Plugins.SemanticAnnotator/SemanticAnnotatorPlugin.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 using Coravel.Scheduling.Schedule.Interfaces;
 using Impostor.Plugins.SemanticAnnotator.Jobs;
 using Microsoft.Extensions.Configuration;
+using Impostor.Plugins.SemanticAnnotator.Models;
+using Microsoft.Extensions.Options;
 
 namespace Impostor.Plugins.SemanticAnnotator
 {
@@ -15,18 +17,22 @@ namespace Impostor.Plugins.SemanticAnnotator
         private readonly ILogger<SemanticAnnotatorPlugin> _logger;
         private readonly IServiceProvider _serviceProvider;
         private readonly IConfiguration _configuration;
+        private readonly bool _useBuffer;
+        private readonly int _annotationIntervalMilliseconds;
 
-        public SemanticAnnotatorPlugin(ILogger<SemanticAnnotatorPlugin> logger, IServiceProvider serviceProvider, IConfiguration configuration)
+        public SemanticAnnotatorPlugin(ILogger<SemanticAnnotatorPlugin> logger, IServiceProvider serviceProvider, IConfiguration _configuration, IOptions<AnnotatorServiceOptions> options)
         {
             _logger = logger;
             _serviceProvider = serviceProvider;
+            _useBuffer = _configuration.GetValue<bool>("UseBufferMode", true);
+            _annotationIntervalMilliseconds = options.Value.AnnotationIntervalMilliseconds;
         }
 
         public override ValueTask EnableAsync()
         {
             _logger.LogInformation("[SemanticAnnotatorPlugin] EnableAsync called.");
 
-            int annotationPeriod = int.Parse(Environment.GetEnvironmentVariable("ANNOTATION_PERIOD") ?? "30");
+        
 
             var scheduler = _serviceProvider.GetRequiredService<IScheduler>();
             bool useBuffer = _configuration.GetValue<bool>("UseBufferMode", true);
@@ -34,21 +40,21 @@ namespace Impostor.Plugins.SemanticAnnotator
             if (useBuffer)
             {
                 scheduler.Schedule<AnnotationJob>()
-                .EverySeconds(annotationPeriod)
+                .EverySeconds(_annotationIntervalMilliseconds)
                 .PreventOverlapping(nameof(AnnotationJob));
 
                 scheduler.Schedule<ArgumentationJob>()
-                    .EverySeconds(annotationPeriod)
+                    .EverySeconds(_annotationIntervalMilliseconds)
                     .PreventOverlapping(nameof(ArgumentationJob));
 
                 scheduler.Schedule<GameNotarizationJob>()
-                    .EverySeconds(annotationPeriod)
+                    .EverySeconds(_annotationIntervalMilliseconds)
                     .PreventOverlapping(nameof(GameNotarizationJob));
             }
             else
             {
                 scheduler.Schedule<DecisionSupportJob>()
-                    .EverySeconds(annotationPeriod)
+                    .EverySeconds(_annotationIntervalMilliseconds)
                     .PreventOverlapping(nameof(DecisionSupportJob));
             }
 
